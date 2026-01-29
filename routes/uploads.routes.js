e8dd591a1e29fbe2f5f468b67e39c2f6b1976c1d
swarnacse19 <sharna.cse9.bu@gmail.com>
2026-01-29 22:48:56 +0600

    create cloudinary file

---
+++ routes/uploads.routes.js
@@ -0,0 +1,13 @@
+// const express = require('express');
+// const upload = require('../config/multer');
+
+// const router = express.Router();
+
+// router.post("/", upload.single("image"), (req, res) => {
+//   res.json({
+//     imageUrl: req.file.path,
+//     publicId: req.file.filename,
+//   });
+// });
+
+// module.exports = router;
