fdb938fdec738d19f95b7d7c5d6f21733ef7d42d
swarnacse19 <sharna.cse9.bu@gmail.com>
2026-01-29 23:12:48 +0600

    create upload routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const userRoutes = require('./routes/user.routes');
 const electionRoutes = require('./routes/election.routes');
 const noticeRoutes = require('./routes/notice.routes');
+const uploadRoutes = require('./routes/uploads.routes');
 
 const { connectDB } = require("./config/db");
 const app = express();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRoutes);
+app.use("/upload", uploadRoutes);
 app.use("/elections", electionRoutes);
 app.use("/notices", noticeRoutes);
 
--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -1,13 +1,13 @@
-// const express = require('express');
-// const upload = require('../config/multer');
+const express = require('express');
+const upload = require('../config/multer');
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post("/", upload.single("image"), (req, res) => {
-//   res.json({
-//     imageUrl: req.file.path,
-//     publicId: req.file.filename,
-//   });
-// });
+router.post("/", upload.single("image"), (req, res) => {
+  res.json({
+    imageUrl: req.file.path,
+    publicId: req.file.filename,
+  });
+});
 
-// module.exports = router;
+module.exports = router;
