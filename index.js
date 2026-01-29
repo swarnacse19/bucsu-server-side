036a15293f6e180e8c11b9aaca6673ae8d699f41
swarnacse19 <sharna.cse9.bu@gmail.com>
2026-01-29 15:05:44 +0600

    create an api for post an election

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const userRoutes = require('./routes/user.routes');
+const electionRoutes = require('./routes/election.routes');
 
 const { connectDB } = require("./config/db");
 const app = express();
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRoutes);
+app.use("/elections", electionRoutes);
 
 app.get("/", (req, res) => {
   res.send("BUCSU server side is running");
---
+++ routes/election.routes.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const { client } = require("../config/db");
+const { ObjectId } = require("mongodb");
+
+const router = express.Router();
+
+const electionsCollection = client
+  .db("BucsuDB")
+  .collection("elections");
+
+router.post("/", async (req, res) => {
+  try {
+    const election = req.body;
+
+    // basic validation
+    if (
+      !election.title ||
+      !election.description ||
+      !election.type ||
+      !election.startDate ||
+      !election.endDate ||
+      !Array.isArray(election.positions)
+    ) {
+      return res.status(400).send({ message: "Missing required fields" });
+    }
+
+    // default fields
+    election.status = "active"; 
+    election.createdAt = new Date();
+
+    const result = await electionsCollection.insertOne(election);
+
+    res.send({
+      message: "Election created successfully",
+      inserted: true,
+      result,
+    });
+  } catch (error) {
+    res.status(500).send({ message: "Failed to create election" });
+  }
+});
+
+
+// router.get("/", async (req, res) => {
+//   const elections = await electionsCollection.find().toArray();
+//   res.send(elections);
+// });
+
+
+// router.get("/:id", async (req, res) => {
+//   const id = req.params.id;
+//   const election = await electionsCollection.findOne({
+//     _id: new ObjectId(id),
+//   });
+//   res.send(election);
+// });
+
+
+// router.patch("/:id", async (req, res) => {
+//   const id = req.params.id;
+//   const update = req.body;
+
+//   const result = await electionsCollection.updateOne(
+//     { _id: new ObjectId(id) },
+//     { $set: update }
+//   );
+
+//   res.send(result);
+// });
+
+
+// router.delete("/:id", async (req, res) => {
+//   const id = req.params.id;
+
+//   const result = await electionsCollection.deleteOne({
+//     _id: new ObjectId(id),
+//   });
+
+//   res.send(result);
+// });
+
+module.exports = router;
